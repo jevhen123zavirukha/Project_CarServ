191bd7a13a3c57b8b46c7ee06ed386ce0810897a
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-10-14 15:29:37 +0200

    nothing

--- Monthly_Project1/settings.py
+++ Monthly_Project1/settings.py
@@ -33,9 +33,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
-# DEBUG = True
 
-# ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 ALLOWED_HOSTS = ['*']
 
 # Application definition
