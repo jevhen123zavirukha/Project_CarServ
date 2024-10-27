fbab34df431cc9f758bcba960dceb16d2b5a4e77
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-10-27 10:21:38 +0100

    none

--- home/models.py
+++ home/models.py
@@ -5,13 +5,13 @@ class Establishment(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=255)
     email = models.EmailField()
     website = models.URLField(blank=True, null=True)
-    years_experience = models.CharField(max_length=20)
-    expert_technicians = models.CharField(max_length=20)
-    satisfies_clients = models.CharField(max_length=20)
-    compleate_projects = models.CharField(max_length=20)
+    years_experience = models.CharField(max_length=255)
+    expert_technicians = models.CharField(max_length=255)
+    satisfies_clients = models.CharField(max_length=255)
+    compleate_projects = models.CharField(max_length=255)
 
     is_visible = models.BooleanField(default=True)
 
